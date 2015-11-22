45b03851dcbacb046da8e74a356c3fe5836d9a48
arduinoenergia <matheart@libero.it>
2015-11-22 23:04:38 +0100

    Aggiunto automa per gestione demo del funzionamento dei servomotori.

--- pwm430_parse.cpp
+++ pwm430_parse.cpp
@@ -10,6 +10,10 @@ pwm SERVO;
 
 void setup() {
   // put your setup code here, to run once:
+	/// SET up serial on usb bus
+	Serial.begin(115200);
+	Serial.println("Benvenuto nel sistema ENERGIA!");
+
 	SERVO.setup(F_CPU, 50);
 	/// set up 4 pwm output
 	SERVO.pinSetting(P1_2);
@@ -21,20 +25,37 @@ void setup() {
 	SERVO.pinValue(P1_2, 75, 10);
 	SERVO.pinValue(P1_2, 75, 10);
 	SERVO.pinValue(P1_2, 75, 10);
-	/// set up serial communication
-	Serial.begin(9600);
+
 }
 
 void loop() {
   // demo of servo motion
-	SERVO.pinValue(P1_2, 0);
-	SERVO.pinValue(P1_3, 0);
-	SERVO.pinValue(P1_4, 0);
-	SERVO.pinValue(P1_5, 0);
-	delay(500);
-	SERVO.pinValue(P1_5, 5);
-	SERVO.pinValue(P1_4, -15);
-	delay(1000);
+	static int stato = 0;
+	switch (stato){
+	case 0:
+		SERVO.pinValue(P1_2, 0);
+		SERVO.pinValue(P1_3, 0);
+		SERVO.pinValue(P1_4, 0);
+		SERVO.pinValue(P1_5, 0);
+		stato = 1;
+	break;
+
+	case 1:
+		delay(500);
+		SERVO.pinValue(P1_5, 5);
+		SERVO.pinValue(P1_4, -15);
+		delay(1000);
+		stato = 2;
+	break;
+
+	case 2:
+		SERVO.pinValue(P1_5, 25);
+		SERVO.pinValue(P1_4, -65);
+		delay(1000);
+		stato = 1;
+	break;
+	}
+
 }
 
 
